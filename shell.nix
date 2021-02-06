837c44429f00a9b1fc0070f8baa259211e6e8258
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2021-02-06 10:33:05 +0100

    Initial commit

---
+++ shell.nix
@@ -0,0 +1,13 @@
+{pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/536fe36e23ab0fc8b7f35c24603422eee9fc17a2.tar.gz") {}
+}:
+with pkgs;
+
+stdenv.mkDerivation {
+  name = "rust-env";
+  nativeBuildInputs = [
+    rustc cargo
+  ];
+  buildInputs = [
+  ];
+
+}
